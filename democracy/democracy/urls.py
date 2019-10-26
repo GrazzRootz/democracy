857a0ad8fec0d0e39b2cf67bebe1efe0f04b77ef
jrawling <rawling.jacob@gmail.com>
2019-10-26 16:00:32 +0100

    (Perhaps) Have auth working

--- democracy/democracy/urls.py
+++ democracy/democracy/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from . import views
 
 router = routers.DefaultRouter()
 # Add views from the apps as they are made
